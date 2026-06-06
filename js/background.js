28f0dea004ecbe2f94f8bcc4975fa1147491bec1
eduardopietre <pietre.eduardo@gmail.com>
2026-06-06 09:45:10 -0300

    Refactor background styles and improve CSS properties for better layout consistency

--- js/background.js
+++ js/background.js
@@ -6,11 +6,17 @@ function applyBackground() {
     el.style.filter = `blur(${AppConfig.bgBlur}px) brightness(${brightness})`;
 
     if (AppConfig.bgType === "2" && AppConfig.bgImage) {
-        el.style.backgroundColor = "";
+        el.style.backgroundColor = "#000000";
         el.style.backgroundImage = `url("${AppConfig.bgImage}")`;
+        el.style.backgroundSize = "auto 100%";
+        el.style.backgroundPosition = "center center";
+        el.style.backgroundRepeat = "no-repeat";
     } else {
         el.style.backgroundImage = "";
         el.style.backgroundColor = AppConfig.bgColor;
+        el.style.backgroundSize = "";
+        el.style.backgroundPosition = "";
+        el.style.backgroundRepeat = "";
     }
 }
 
